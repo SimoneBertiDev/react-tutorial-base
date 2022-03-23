3b3200ec32f8cfe49b6b6da8c4ec4a69d5d1b68f
Simone_Berti <foto.berti55@gmail.com>
2022-03-23 17:13:43 +0100

    Appuntamenti
    Visualizzare tutti gli appuntamenti di una lista.
    Rimuovere tutti gli appuntamenti di una lista.
    Aggiungere nuovamente tutti gli appuntamenti della lista.
    Rimuovere singolarmente un solo appuntamento dalla lista

--- appuntamenti/inizio/src/App.js
+++ appuntamenti/inizio/src/App.js
@@ -1,11 +1,36 @@
 import List from "./List";
 import data from "./data";
-
+import { useState } from "react";
 function App() {
+  const [people, setPeople] = useState(data);
+  // console.log(people);
+
+  const ReloadAllItem = () => {
+    setPeople(data);
+  };
+
+  const RemoveItem = (id) => {
+    setPeople((oldValue) => {
+      return oldValue.filter((value) => value.id !== id);
+    });
+  };
   return (
-    <div className="App">
-      <h2> App</h2>
-    </div>
+    <section>
+      <div className="container">
+        <h2 style={{ color: "var(--bg-blue)" }}>Prossimi incontri</h2>
+        <div className="people-list">
+          <List data={people} RemoveItem={RemoveItem} />
+        </div>
+        <div className="btn-group">
+          <button className="btn btn-reset" onClick={ReloadAllItem}>
+            Reload
+          </button>
+          <button className="btn btn-delete" onClick={() => setPeople([])}>
+            Delete all
+          </button>
+        </div>
+      </div>
+    </section>
   );
 }
 
--- appuntamenti/inizio/src/List.js
+++ appuntamenti/inizio/src/List.js
@@ -1,11 +1,35 @@
 import React from "react";
-
-const List = () => {
-  return <ul></ul>;
+import { RiDeleteBack2Fill } from "react-icons/ri";
+const List = (props) => {
+  // console.log(props);
+  return (
+    <ul className="user-list">
+      {props.data.map((person) => {
+        return (
+          <li key={person.id}>
+            <Person {...person} removeItem={props.RemoveItem} />
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
-const Person = () => {
-  return <article>perosna</article>;
+const Person = ({ id, nome, stato, img, removeItem }) => {
+  return (
+    <article>
+      <img src={img} alt="person-img" className="person-img" />
+      <div className="person-info">
+        <div className="person-action">
+          <h4>{nome}</h4>
+          <button className="btn" onClick={() => removeItem(id)}>
+            <RiDeleteBack2Fill className="icon" />
+          </button>
+        </div>
+        <p>{stato}</p>
+      </div>
+    </article>
+  );
 };
 
 export default List;
