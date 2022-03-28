7b3ff34f6add27c53f9f239ca349ecae798d777d
Simone_Berti <foto.berti55@gmail.com>
2022-03-28 12:49:17 +0200

    Progetto 5 la gelateria
    Usando dei tati fake della nostra api abbiamo creato il componente gelato che è composto dai dati fetcahti per poi andare a filtrare per categoria

--- gelateria/inizio/src/App.js
+++ gelateria/inizio/src/App.js
@@ -1,7 +1,16 @@
 import { Menu } from "./components";
 
 function App() {
-  return <div className="App">APP</div>;
+  return (
+    <div className="App">
+      <section className="section-center">
+        <h3>
+          Nice Cream
+          <Menu />
+        </h3>
+      </section>
+    </div>
+  );
 }
 
 export default App;
--- gelateria/inizio/src/components/Menu.js
+++ gelateria/inizio/src/components/Menu.js
@@ -6,7 +6,123 @@ import data from "../fakeData";
 const url = "https://react-corso-api.netlify.app/.netlify/functions/gelateria";
 
 const Menu = () => {
-  return <div className="container">MENU</div>;
+  //Loading state for data fatching
+  const [isLoadind, setIsLoadind] = useState(true);
+
+  //error Heandling state
+  const [isError, setError] = useState(false);
+
+  //tutti i Prodotti
+  const [prodotti, setProdotti] = useState(data);
+
+  //active BTN selector
+  const [selected, setSelected] = useState(0);
+
+  //Prodotti filter from the prodotti state
+  const [filterProducts, setFilterProducts] = useState();
+
+  //categorie di prodotto
+  const [categorie, setCategorie] = useState([]);
+
+  //Categorie di prodotti
+  // const categoria = Array.from(new Set(prodotti.map((el) => el.categoria)));
+  // categoria.unshift("all");
+
+  const filtraPro = (categoria, index) => {
+    setSelected(index);
+    if (categoria === "all") {
+      setFilterProducts(prodotti);
+    } else {
+      setFilterProducts(
+        prodotti.filter((el) => (el.categoria === categoria ? el : ""))
+      );
+    }
+  };
+
+  React.useEffect(() => {
+    //Funzione eseguita immediatamente
+    (async () => {
+      setIsLoadind(true);
+      setError(false);
+      try {
+        const response = await axios.get(url);
+        // console.log(response.data);
+        setProdotti(response.data.data.default);
+        setFilterProducts(response.data.data.default);
+
+        //categorie Array
+        const nuoveCategorie = Array.from(
+          new Set(response.data.data.default.map((el) => el.categoria))
+        );
+        nuoveCategorie.unshift("all");
+        setCategorie(nuoveCategorie);
+        setIsLoadind(false);
+        setError(false);
+      } catch (error) {
+        console.log(error);
+        setIsLoadind(false);
+        setError(true);
+      }
+    })();
+  }, []);
+
+
+
+  // console.log(categoria);
+  // console.log(prodotti);
+  return (
+    <div className="container">
+      <h4 style={{ textAlign: "center", textTransform: "uppercase" }}>
+        Le nostre scelte
+      </h4>
+      {!isLoadind && !isError ? (
+        <>
+          <div className="lista-categorie">
+            {categorie.map((categoria, index) => {
+              return (
+                <button
+                  key={index}
+                  onClick={() => filtraPro(categoria, index)}
+                  className={`btn btn-selector ${
+                    index === selected && "active"
+                  }`}
+                >
+                  {categoria}
+                </button>
+              );
+            })}
+          </div>
+          <div className="vetrina">
+            {filterProducts.map((el) => {
+              return <Gelato key={el.id} {...el} />;
+            })}
+          </div>
+        </>
+      ) : !isLoadind && isError ? (
+        <h4
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "trasform(-50%, -50%",
+          }}
+        >
+          Error . . .
+        </h4>
+      ) : (
+        <h4
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "trasform(-50%, -50%",
+          }}
+        >
+          Loading . . .
+        </h4>
+      )}
+    </div>
+  );
 };
 
 export default Menu;
