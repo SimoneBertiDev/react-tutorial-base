3da79e6b1036daeeeac995eafa81dd37900e52e3
Simone_Berti <foto.berti55@gmail.com>
2022-03-28 11:10:08 +0200

    Da finire progetto 4 Slider
    Mancano ancora le stelle da inerire e lo slider in automatico

--- slider/inizio/src/components/Slide.js
+++ slider/inizio/src/components/Slide.js
@@ -1,6 +1,14 @@
 import React from "react";
-const Slide = () => {
-  return <div>Slide</div>;
+const Slide = ({ autore, recensione, voto , classes}) => {
+  return (
+    <article className={`slide ${classes}`}>
+      <div className="review">
+        <h4>{autore}</h4>
+        <p>{recensione}</p>
+        <div className="star-container">{voto}</div>
+      </div>
+    </article>
+  );
 };
 
 export default Slide;
--- slider/inizio/src/components/Slider.js
+++ slider/inizio/src/components/Slider.js
@@ -1,8 +1,64 @@
 import React, { useState, useEffect } from "react";
 import Slide from "./Slide";
 import data from "../data";
+
 const Slider = () => {
-  return <div className="container slider">Slider</div>;
+  const [recensioni, setRecensioni] = useState(data);
+  const [active, setActive] = useState(0);
+
+  //passa alla prossima slide
+  const prossimaSLide = () => {
+    setActive((prevValue) => {
+      if (prevValue + 1 > recensioni.length - 1) {
+        return 0;
+      } else {
+        return prevValue + 1;
+      }
+    });
+  };
+  //passa alla precedente slide
+  const precedenteSLide = () => {
+    setActive((prevValue) => {
+      if (prevValue - 1 < 0) {
+        return recensioni.length - 1;
+      } else {
+        return prevValue - 1;
+      }
+    });
+  };
+
+  return (
+    <div className="container slider">
+      {recensioni.map((recensione, index) => {
+        let positionClass = "";
+        if (index === active) {
+          positionClass = "active";
+        } else if (
+          index + 1 === active ||
+          (active === 0 && index === recensioni.length - 1)
+        ) {
+          positionClass = "prev";
+        } else {
+          positionClass = "next";
+        }
+
+        return (
+          <Slide key={recensione.id} {...recensione} classes={positionClass} />
+        );
+      })}
+      <div className="btn-group slider-btn-group">
+        <button
+          className="btn btn-slider perv-slider"
+          onClick={precedenteSLide}
+        >
+          perv
+        </button>
+        <button className="btn btn-slider next-slider" onClick={prossimaSLide}>
+          next
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Slider;
